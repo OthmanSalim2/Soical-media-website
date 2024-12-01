129dd230e4c66f214ea5bce1e5818f91482e09cb
othman salim <othmansalim423@gmail.com>
2024-12-01 12:39:43 +0200

    Add migrations for database schema

--- database/migrations/2024_11_30_171705_create_groups_table.php
+++ database/migrations/2024_11_30_171705_create_groups_table.php
@@ -10,13 +10,17 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->id();
-            $table->longText('body')->nullable();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('cover_path', 1024)->nullable();
+            $table->string('thumbnail_path', 1024)->nullable();
+            $table->boolean('auto_approval')->default(true);
+            $table->string('about')->nullable();
             $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('group_id')->nullable()->constrained('groups');
-            $table->foreignId('deleted_by')->nullable()->constrained('users');
             $table->timestamp('deleted_at')->nullable();
+            $table->foreignId('deleted_by')->nullable()->constrained('users');
             $table->timestamps();
         });
     }
@@ -26,6 +30,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('groups');
     }
 };
--- database/migrations/2024_11_30_171740_create_post_attachments_table.php
+++ database/migrations/2024_11_30_171740_create_post_attachments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,13 @@ return new class extends Migration
     {
         Schema::create('post_attachments', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('post_id')->constrained('posts');
+            $table->string('name'); // test.png
+            $table->string('path');
+            $table->string('mime', 25); // image/png
+            $table->integer('size'); // 10000
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
