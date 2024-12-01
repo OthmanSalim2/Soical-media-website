0b0a867e8a8fbcc6e78a0894148a323653a14b87
othman salim <othmansalim423@gmail.com>
2024-12-01 18:27:42 +0200

    clean unnecessary routes and view files

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {Head} from '@inertiajs/vue3';
+
+defineProps({});
+
+</script>
+
+<template>
+    <Head title="Social Media Website"/>
+    
+</template>
+
+<style>
+
+</style>
